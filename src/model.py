43a3a473e863892257ad2248b2d07c9550b20165
PRADEEP-DScientist <rockyshady721@gmail.com>
2021-09-16 10:07:40 +0530

    Tracker Added

--- src/model.py
+++ src/model.py
@@ -8,7 +8,7 @@ import warnings
 import sys
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import ElasticNet
 import argparse
 import joblib
@@ -16,12 +16,12 @@ import json
 from get_data import read_params
 
 
-def eval_metrics(actual,pred):
-    rmse = np.sqrt(mean_squared_error(actual,pred))
-    mae = mean_absolute_error(actual,pred)
-    r2 = r2_score(actual,pred)
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
 
-    return rmse,mae,r2
+    return rmse, mae, r2
 
 
 def train_and_evaluate(config_path):
@@ -31,26 +31,48 @@ def train_and_evaluate(config_path):
     random_state = config['base']['random_state']
     model_dir = config['model_dir']
 
-    alpha=config['estimators']['ElasticNet']['params']['alpha']
-    l1_ratio=config['estimators']['ElasticNet']['params']['l1_ratio']
+    alpha = config['estimators']['ElasticNet']['params']['alpha']
+    l1_ratio = config['estimators']['ElasticNet']['params']['l1_ratio']
 
     target = config["base"]['target_col']
+
+    scores_file = config['reports']['scores']
+    params_file = config['reports']['params']
 
     train = pd.read_csv(train_data_path, sep=',', encoding='utf-8')
     val = pd.read_csv(val_data_path, sep=',', encoding='utf-8')
 
-    train_X = train.drop(target,axis=1)
+    train_X = train.drop(target, axis=1)
     val_X = val.drop(target, axis=1)
 
     train_y = train[target]
     val_y = val[target]
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state= random_state)
-    lr.fit(train_X,train_y)
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+    lr.fit(train_X, train_y)
 
     prediction = lr.predict(val_X)
 
     (rmse, mae, r2) = eval_metrics(val_y, prediction)
+
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2_score": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio
+        }
+        json.dump(params, f, indent=4)
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+    joblib.dump(lr,model_path)
 
 
 if __name__ == "__main__":
